f8cf3acb2e48024b355143073cf56ddb23451f96
BenSibley <ben@competethemes.com>
2015-01-27 11:03:04 -0500

    comments and customizer updates

--- comments.php
+++ comments.php
@@ -1,7 +1,22 @@
 <?php
 /* If a post password is required or no comments are given and comments/pings are closed, return. */
 if ( post_password_required() || ( !have_comments() && !comments_open() && !pings_open() ) )
+    return;
+
+// get user's comment display setting
+$comments_display = get_theme_mod('comments_display');
+
+// get the current post type
+$post_type = get_post_type();
+
+// error prevention
+if( is_array( $comments_display ) ) {
+
+    // if the current post type is not included, don't display comments
+    if( ! in_array( $post_type, $comments_display ) ) {
         return;
+    }
+}
 
 if ( comments_open() ) { ?>
 
@@ -11,63 +26,62 @@ if ( comments_open() ) { ?>
                 <?php comments_number( __( 'Be First to Comment', 'author' ), __( 'One Comment', 'author'), __( '% Comments', 'author' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'7') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <?php comment_form(); ?>
+        <ol class="comment-list">
+            <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'3') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <?php comment_form(); ?>
+    </section><!-- #comments -->
 <?php
 } elseif(!comments_open() && have_comments() && pings_open() ) { ?>
     <section id="comments" class="comments">
         <div class="comments-number">
             <h3>
-<?php comments_number( __( 'Be First to Comment', 'author' ), __( 'One Comment', 'author'), __( '% Comments', 'author' ) ); ?>
+                <?php comments_number( __( 'Be First to Comment', 'author' ), __( 'One Comment', 'author'), __( '% Comments', 'author' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'3') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <p class="comments-closed pings-open">
-                    <?php printf( __( 'Comments are closed, but <a href="%s" title="Trackback URL for this post">trackbacks</a> and pingbacks are open.', 'author' ), esc_url( get_trackback_url() ) ); ?>
-            </p><!-- .comments-closed .pings-open -->
+        <ol class="comment-list">
+            <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'3') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <p class="comments-closed pings-open">
+            <?php printf( __( 'Comments are closed, but <a href="%s" title="Trackback URL for this post">trackbacks</a> and pingbacks are open.', 'author' ), esc_url( get_trackback_url() ) ); ?>
+        </p><!-- .comments-closed .pings-open -->
+    </section><!-- #comments -->
 <?php
-} elseif ( !comments_open() && have_comments() ) { ?>
+} elseif ( ! comments_open() && have_comments() ) { ?>
     <section id="comments" class="comments">
         <div class="comments-number">
             <h3>
-<?php comments_number( __( 'Be First to Comment', 'author' ), __( 'One Comment', 'author'), __( '% Comments', 'author' ) ); ?>
+                <?php comments_number( __( 'Be First to Comment', 'author' ), __( 'One Comment', 'author'), __( '% Comments', 'author' ) ); ?>
             </h3>
         </div>
-            <ol class="comment-list">
-                    <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'3') ); ?>
-            </ol><!-- .comment-list -->
-            <?php    
-            if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
-                <nav class="comment-pagination">
-                    <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
-                </nav>
-            <?php } ?>
-            <p class="comments-closed">
-                <?php _e( 'Comments are closed.', 'author' ); ?>
-            </p><!-- .comments-closed -->
-<?php 
-} else { ?>
-
-    <p class="comments-closed">
+        <ol class="comment-list">
+            <?php wp_list_comments(array( 'callback'=>'ct_author_customize_comments', 'max_depth'=>'3') ); ?>
+        </ol><!-- .comment-list -->
+        <?php
+        if( (get_option('page_comments') == 1) && (get_comment_pages_count() > 1) ) { ?>
+            <nav class="comment-pagination">
+                <p class="previous-comment"><?php previous_comments_link(); ?></p><p class="next-comment"><?php next_comments_link(); ?></p>
+            </nav>
+        <?php } ?>
+        <p class="comments-closed">
             <?php _e( 'Comments are closed.', 'author' ); ?>
-    </p><!-- .comments-closed -->
-
-<?php } ?>
-  
+        </p><!-- .comments-closed -->
     </section><!-- #comments -->
+<?php
+} else { ?>
+    <p class="comments-closed">
+        <?php _e( 'Comments are closed.', 'author' ); ?>
+    </p><!-- .comments-closed -->
+<?php } ?>
--- inc/customizer.php
+++ inc/customizer.php
@@ -1,9 +1,9 @@
 <?php
 
 /* Add customizer panels, sections, settings, and controls */
-add_action( 'customize_register', 'ct_unlimited_add_customizer_content' );
+add_action( 'customize_register', 'ct_author_add_customizer_content' );
 
-function ct_unlimited_add_customizer_content( $wp_customize ) {
+function ct_author_add_customizer_content( $wp_customize ) {
 
 	/***** Add PostMessage Support *****/
 	
@@ -14,7 +14,7 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 	/***** Add Custom Controls *****/
 
 	// create url input control
-	class ct_unlimited_url_input_control extends WP_Customize_Control {
+	class ct_author_url_input_control extends WP_Customize_Control {
 		// create new type called 'url'
 		public $type = 'url';
 		// the content to be output in the Customizer
@@ -28,11 +28,34 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 		}
 	}
 
+	// create multi-checkbox/select control
+	class ct_author_Multi_Checkbox_Control extends WP_Customize_Control {
+		public $type = 'multi-checkbox';
+
+		public function render_content() {
+
+			if ( empty( $this->choices ) )
+				return;
+			?>
+			<label>
+				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<select id="comment-display-control" <?php $this->link(); ?> multiple="multiple" style="height: 100%;">
+					<?php
+					foreach ( $this->choices as $value => $label ) {
+						$selected = ( in_array( $value, $this->value() ) ) ? selected( 1, 1, false ) : '';
+						echo '<option value="' . esc_attr( $value ) . '"' . $selected . '>' . $label . '</option>';
+					}
+					?>
+				</select>
+			</label>
+		<?php }
+	}
+
 	/***** Logo Upload *****/
 
 	// section
-	$wp_customize->add_section( 'ct_unlimited_logo_upload', array(
-		'title'      => __( 'Logo Upload', 'unlimited' ),
+	$wp_customize->add_section( 'ct_author_logo_upload', array(
+		'title'      => __( 'Logo Upload', 'author' ),
 		'priority'   => 30,
 		'capability' => 'edit_theme_options'
 	) );
@@ -47,8 +70,8 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 	// control
 	$wp_customize->add_control( new WP_Customize_Image_Control(
 		$wp_customize, 'logo_image', array(
-			'label'    => __( 'Upload custom logo.', 'unlimited' ),
-			'section'  => 'ct_unlimited_logo_upload',
+			'label'    => __( 'Upload custom logo.', 'author' ),
+			'section'  => 'ct_author_logo_upload',
 			'settings' => 'logo_upload',
 		)
 	) );
@@ -56,47 +79,47 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 	/***** Social Media Icons *****/
 
 	// get the social sites array
-	$social_sites = ct_unlimited_social_site_list();
+	$social_sites = ct_author_social_array();
 
 	// set a priority used to order the social sites
 	$priority = 5;
 
 	// section
-	$wp_customize->add_section( 'ct_unlimited_social_media_icons', array(
-		'title'          => __('Social Media Icons', 'unlimited'),
-		'priority'       => 35,
+	$wp_customize->add_section( 'ct_author_social_media_icons', array(
+		'title'          => __('Social Media Icons', 'author'),
+		'priority'       => 25,
 	) );
 
 	// create a setting and control for each social site
-	foreach( $social_sites as $social_site ) {
+	foreach( $social_sites as $social_site => $value ) {
 		// if email icon
 		if( $social_site == 'email' ) {
 			// setting
 			$wp_customize->add_setting( "$social_site", array(
 				'type'              => 'theme_mod',
 				'capability'        => 'edit_theme_options',
-				'sanitize_callback' => 'ct_unlimited_sanitize_email',
+				'sanitize_callback' => 'ct_author_sanitize_email',
 				'transport'         => 'postMessage'
 			) );
 			// control
 			$wp_customize->add_control( $social_site, array(
-				'label'   => $social_site . " " . __("address:", 'unlimited' ),
-				'section' => 'ct_unlimited_social_media_icons',
+				'label'   => $social_site . ' ' . __('address:', 'author' ),
+				'section' => 'ct_author_social_media_icons',
 				'priority'=> $priority,
 			) );
 		} else {
 			// setting
-			$wp_customize->add_setting( "$social_site", array(
+			$wp_customize->add_setting( $social_site, array(
 				'type'              => 'theme_mod',
 				'capability'        => 'edit_theme_options',
 				'sanitize_callback' => 'esc_url_raw',
 				'transport'         => 'postMessage'
 			) );
 			// control
-			$wp_customize->add_control( new ct_unlimited_url_input_control(
+			$wp_customize->add_control( new ct_author_url_input_control(
 				$wp_customize, $social_site, array(
-					'label'   => $social_site . " " . __("url:", 'unlimited' ),
-					'section' => 'ct_unlimited_social_media_icons',
+					'label'   => $social_site . ' ' . __('url:', 'author' ),
+					'section' => 'ct_author_social_media_icons',
 					'priority'=> $priority,
 				)
 			) );
@@ -108,8 +131,8 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 	/***** Search Bar *****/
 
 	// section
-	$wp_customize->add_section( 'ct_unlimited_search_bar', array(
-		'title'      => __( 'Search Bar', 'unlimited' ),
+	$wp_customize->add_section( 'ct_author_search_bar', array(
+		'title'      => __( 'Search Bar', 'author' ),
 		'priority'   => 40,
 		'capability' => 'edit_theme_options'
 	) );
@@ -118,18 +141,49 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
 		'default'           => 'show',
 		'type'              => 'theme_mod',
 		'capability'        => 'edit_theme_options',
-		'sanitize_callback' => 'ct_unlimited_sanitize_all_show_hide_settings'
+		'sanitize_callback' => 'ct_author_sanitize_all_show_hide_settings'
 	) );
 	// control
 	$wp_customize->add_control( 'search_bar', array(
 		'type' => 'radio',
-		'label' => __('Show search bar at top of site?', 'unlimited'),
-		'section' => 'ct_unlimited_search_bar',
+		'label' => __('Show search bar at top of site?', 'author'),
+		'section' => 'ct_author_search_bar',
 		'setting' => 'search_bar',
 		'choices' => array(
-			'show' => __('Show', 'unlimited'),
-			'hide' => __('Hide', 'unlimited')
+			'show' => __('Show', 'author'),
+			'hide' => __('Hide', 'author')
 		),
+	) );
+
+	/***** Comment Display *****/
+
+	// section
+	$wp_customize->add_section( 'ct_author_comments_display', array(
+		'title'      => __( 'Comment Display', 'author' ),
+		'priority'   => 65,
+		'capability' => 'edit_theme_options'
+	) );
+	// setting
+	$wp_customize->add_setting( 'comments_display', array(
+		'default'           => 'none',
+		'type'              => 'theme_mod',
+		'capability'        => 'edit_theme_options',
+		'sanitize_callback' => 'ct_author_sanitize_comments_setting',
+	) );
+	// control
+	$wp_customize->add_control( new ct_author_Multi_Checkbox_Control(
+		$wp_customize, 'comments_display', array(
+			'label'          => __( 'Show comments on:', 'author' ),
+			'section'        => 'ct_author_comments_display',
+			'settings'       => 'comments_display',
+			'type'           => 'multi-checkbox',
+			'choices'        => array(
+				'post'   => __('Posts', 'author'),
+				'page'  => __('Pages', 'author'),
+				'attachment'  => __('Attachments', 'author'),
+				'none'  => __('Do not show', 'author')
+			)
+		)
 	) );
 }
 
@@ -139,11 +193,11 @@ function ct_unlimited_add_customizer_content( $wp_customize ) {
  * Sanitize settings with show/hide as options
  * Used in: search bar
  */
-function ct_unlimited_sanitize_all_show_hide_settings($input){
+function ct_author_sanitize_all_show_hide_settings($input){
 	// create array of valid values
 	$valid = array(
-		'show' => __('Show', 'unlimited'),
-		'hide' => __('Hide', 'unlimited')
+		'show' => __('Show', 'author'),
+		'hide' => __('Hide', 'author')
 	);
 	// if returned data is in array use it, else return nothing
 	if ( array_key_exists( $input, $valid ) ) {
@@ -157,7 +211,30 @@ function ct_unlimited_sanitize_all_show_hide_settings($input){
  * sanitize email address
  * Used in: Social Media Icons
  */
-function ct_unlimited_sanitize_email( $input ) {
+function ct_author_sanitize_email( $input ) {
 
 	return sanitize_email( $input );
+}
+
+// sanitize comment display multi-check
+function ct_author_sanitize_comments_setting($input){
+
+	// valid data
+	$valid = array(
+		'post'   => __('Posts', 'author'),
+		'page'  => __('Pages', 'author'),
+		'attachment'  => __('Attachments', 'author'),
+		'none'  => __('Do not show', 'author')
+	);
+
+	// loop through array
+	foreach( $input as $selection ) {
+
+		// if it's in the valid data, return it
+		if ( array_key_exists( $selection, $valid ) ) {
+			return $input;
+		} else {
+			return '';
+		}
+	}
 }
