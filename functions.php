e5ed92f3e23ec1ef68ae99d075eec6d74fb9aaa9
BenSibley <ben@competethemes.com>
2015-01-27 10:48:11 -0500

    updated prefix & text domain, and other cleanup in functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once( trailingslashit( get_template_directory() ) . 'library/hybrid.php'
 new Hybrid();
 
 // theme setup
-function ct_unlimited_theme_setup() {
+function ct_author_theme_setup() {
 	
     /* Get action/filter hook prefix. */
 	$prefix = hybrid_get_prefix();
@@ -29,38 +29,38 @@ function ct_unlimited_theme_setup() {
 	}
 
 	// load text domain
-	load_theme_textdomain('unlimited', get_template_directory() . '/languages');
+	load_theme_textdomain('author', get_template_directory() . '/languages');
 
 	// register Primary menu
     register_nav_menus(array(
-        'primary' => __('Primary', 'unlimited')
+        'primary' => __('Primary', 'author')
     ));
 }
-add_action( 'after_setup_theme', 'ct_unlimited_theme_setup', 10 );
+add_action( 'after_setup_theme', 'ct_author_theme_setup', 10 );
 
 // turn off cleaner gallery if Jetpack gallery functions being used
-function ct_unlimited_remove_cleaner_gallery() {
+function ct_author_remove_cleaner_gallery() {
 
 	if( class_exists( 'Jetpack' ) && ( Jetpack::is_module_active( 'carousel' ) || Jetpack::is_module_active( 'tiled-gallery' ) ) ) {
 		remove_theme_support( 'cleaner-gallery' );
 	}
 }
-add_action( 'after_setup_theme', 'ct_unlimited_remove_cleaner_gallery', 11 );
+add_action( 'after_setup_theme', 'ct_author_remove_cleaner_gallery', 11 );
 
 // register widget areas
-function ct_unlimited_register_widget_areas(){
+function ct_author_register_widget_areas(){
 
     /* register after post content widget area */
     hybrid_register_sidebar( array(
-        'name'         => __( 'Primary Sidebar', 'unlimited' ),
+        'name'         => __( 'Primary Sidebar', 'author' ),
         'id'           => 'sidebar',
-        'description'  => __( 'Widgets in this area will be shown in the sidebar next to the main post content', 'unlimited' )
+        'description'  => __( 'Widgets in this area will be shown in the sidebar', 'author' )
     ) );
 }
-add_action('widgets_init','ct_unlimited_register_widget_areas');
+add_action('widgets_init','ct_author_register_widget_areas');
 
 /* added to customize the comments. Same as default except -> added use of gravatar images for comment authors */
-function ct_unlimited_customize_comments( $comment, $args, $depth ) {
+function ct_author_customize_comments( $comment, $args, $depth ) {
     $GLOBALS['comment'] = $comment;
     global $post;
     ?>
@@ -70,7 +70,7 @@ function ct_unlimited_customize_comments( $comment, $args, $depth ) {
                 <?php
                 // if is post author
                 if( $comment->user_id === $post->post_author ) {
-                    ct_unlimited_profile_image_output();
+                    ct_author_profile_image_output();
                 } else {
                     echo get_avatar( get_comment_author_email(), 48 );
                 }
@@ -78,24 +78,23 @@ function ct_unlimited_customize_comments( $comment, $args, $depth ) {
                 <div>
                     <div class="author-name"><?php comment_author_link(); ?></div>
                     <div class="comment-date"><?php comment_date('n/j/Y'); ?></div>
-                    <?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'unlimited' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+                    <?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'author' ), 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
                     <?php edit_comment_link( 'edit' ); ?>
                 </div>    
             </div>
             <div class="comment-content">
                 <?php if ($comment->comment_approved == '0') : ?>
-                    <em><?php _e('Your comment is awaiting moderation.', 'unlimited') ?></em>
+                    <em><?php _e('Your comment is awaiting moderation.', 'author') ?></em>
                     <br />
                 <?php endif; ?>
                 <?php comment_text(); ?>
             </div>
         </article>
-    </li>
     <?php
 }
 
 /* added HTML5 placeholders for each default field and aria-required to required */
-function ct_unlimited_update_fields($fields) {
+function ct_author_update_fields($fields) {
 
 	// get commenter object
     $commenter = wp_get_current_commenter();
@@ -115,52 +114,52 @@ function ct_unlimited_update_fields($fields) {
 
     $fields['author'] =
         '<p class="comment-form-author">
-            <label class="screen-reader-text">' . __("Your Name", "unlimited") . '</label>
-            <input placeholder="' . __("Your Name", "unlimited") . $label . '" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+            <label class="screen-reader-text">' . __("Your Name", "author") . '</label>
+            <input placeholder="' . __("Your Name", "author") . $label . '" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
         '" size="30" ' . $aria_req . ' />
     	</p>';
 
     $fields['email'] =
         '<p class="comment-form-email">
-            <label class="screen-reader-text">' . __("Your Email", "unlimited") . '</label>
-            <input placeholder="' . __("Your Email", "unlimited") . $label . '" id="email" name="email" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+            <label class="screen-reader-text">' . __("Your Email", "author") . '</label>
+            <input placeholder="' . __("Your Email", "author") . $label . '" id="email" name="email" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
         '" size="30" ' . $aria_req . ' />
     	</p>';
 
     $fields['url'] =
         '<p class="comment-form-url">
-            <label class="screen-reader-text">' . __("Your Website URL", "unlimited") . '</label>
-            <input placeholder="' . __("Your URL", "unlimited") . ' (optional)" id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
+            <label class="screen-reader-text">' . __("Your Website URL", "author") . '</label>
+            <input placeholder="' . __("Your URL", "author") . ' (optional)" id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
         '" size="30" />
             </p>';
 
     return $fields;
 }
-add_filter('comment_form_default_fields','ct_unlimited_update_fields');
+add_filter('comment_form_default_fields','ct_author_update_fields');
 
-function ct_unlimited_update_comment_field($comment_field) {
+function ct_author_update_comment_field($comment_field) {
 	
 	$comment_field =
         '<p class="comment-form-comment">
-            <label class="screen-reader-text">' . __("Your Comment", "unlimited") . '</label>
-            <textarea required placeholder="' . __("Enter Your Comment", "unlimited") . '&#8230;" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
+            <label class="screen-reader-text">' . __("Your Comment", "author") . '</label>
+            <textarea required placeholder="' . __("Enter Your Comment", "author") . '&#8230;" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
         </p>';
 	
 	return $comment_field;
 }
-add_filter('comment_form_field_comment','ct_unlimited_update_comment_field');
+add_filter('comment_form_field_comment','ct_author_update_comment_field');
 
 // remove allowed tags text after comment form
-function ct_unlimited_remove_comments_notes_after($defaults){
+function ct_author_remove_comments_notes_after($defaults){
 
     $defaults['comment_notes_after']='';
     return $defaults;
 }
 
-add_action('comment_form_defaults', 'ct_unlimited_remove_comments_notes_after');
+add_action('comment_form_defaults', 'ct_author_remove_comments_notes_after');
 
 // excerpt handling
-function ct_unlimited_excerpt() {
+function ct_author_excerpt() {
 
     // make post variable available
     global $post;
@@ -173,7 +172,7 @@ function ct_unlimited_excerpt() {
 
     // use the read more link if present
     if($ismore) {
-        the_content( __('Read More', 'unlimited') . "<span class='screen-reader-text'>" . get_the_title() . "</span>");
+        the_content( __('Continue reading', 'author') . "<span class='screen-reader-text'>" . get_the_title() . "</span>");
     }
     // otherwise the excerpt is automatic, so output it
     else {
@@ -182,36 +181,37 @@ function ct_unlimited_excerpt() {
 }
 
 // filter the link on excerpts
-function ct_unlimited_excerpt_read_more_link($output) {
+function ct_author_excerpt_read_more_link($output) {
 	global $post;
-	return $output . "<p><a class='more-link' href='". get_permalink() ."'>" . __('Read More', 'unlimited') . "<span class='screen-reader-text'>" . get_the_title() . "</span></a></p>";
+	return $output . "<p><a class='more-link' href='". get_permalink() ."'>" . __('Continue reading', 'author') . "<span class='screen-reader-text'>" . get_the_title() . "</span></a></p>";
 }
 
-add_filter('the_excerpt', 'ct_unlimited_excerpt_read_more_link');
+add_filter('the_excerpt', 'ct_author_excerpt_read_more_link');
 
 // switch [...] to ellipsis on automatic excerpt
-function ct_unlimited_new_excerpt_more( $more ) {
+function ct_author_new_excerpt_more( $more ) {
 	return '&#8230;';
 }
-add_filter('excerpt_more', 'ct_unlimited_new_excerpt_more');
+add_filter('excerpt_more', 'ct_author_new_excerpt_more');
 
 // turns of the automatic scrolling to the read more link 
-function ct_unlimited_remove_more_link_scroll( $link ) {
+function ct_author_remove_more_link_scroll( $link ) {
 	$link = preg_replace( '|#more-[0-9]+|', '', $link );
 	return $link;
 }
-add_filter( 'the_content_more_link', 'ct_unlimited_remove_more_link_scroll' );
+add_filter( 'the_content_more_link', 'ct_author_remove_more_link_scroll' );
 
 // Adds navigation through pages in the loop
-function ct_unlimited_post_navigation() {
+function ct_author_post_navigation() {
     if ( current_theme_supports( 'loop-pagination' ) ) loop_pagination();
 }
 
 // for displaying featured images
-function ct_unlimited_featured_image() {
+function ct_author_featured_image() {
 
 	// get post object
 	global $post;
+
 	// default to no featured image
 	$has_image = false;
 
@@ -240,20 +240,10 @@ function ct_unlimited_featured_image() {
 	}
 }
 
-// puts site title & description in the title tag on front page
-function ct_unlimited_add_homepage_title( $title )
-{
-    if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-        return get_bloginfo( 'title' ) . ' | ' . get_bloginfo( 'description' );
-    }
-    return $title;
-}
-add_filter( 'wp_title', 'ct_unlimited_add_homepage_title' );
-
-function ct_unlimited_add_editor_styles() {
+function ct_author_add_editor_styles() {
     add_editor_style( 'styles/custom-editor-style.css' );
 }
-add_action( 'init', 'ct_unlimited_add_editor_styles' );
+add_action( 'init', 'ct_author_add_editor_styles' );
 
 // fix for bug with Disqus saying comments are closed
 if ( function_exists( 'dsq_options' ) ) {
@@ -262,52 +252,52 @@ if ( function_exists( 'dsq_options' ) ) {
 }
 
 // list social media sites
-function ct_unlimited_social_site_list(){
+function ct_author_social_site_list(){
 
     $social_sites = array('twitter', 'facebook', 'google-plus', 'flickr', 'pinterest', 'youtube', 'vimeo', 'tumblr', 'dribbble', 'rss', 'linkedin', 'instagram', 'reddit', 'soundcloud', 'spotify', 'vine','yahoo', 'behance', 'codepen', 'delicious', 'stumbleupon', 'deviantart', 'digg', 'git', 'hacker-news', 'steam', 'vk', 'weibo', 'tencent-weibo', 'email' );
     return $social_sites;
 }
 
 // associative array of social media sites
-function ct_unlimited_social_array(){
+function ct_author_social_array(){
 
 	$social_sites = array(
-		'twitter' => 'unlimited_twitter_profile',
-		'facebook' => 'unlimited_facebook_profile',
-		'googleplus' => 'unlimited_googleplus_profile',
-		'pinterest' => 'unlimited_pinterest_profile',
-		'linkedin' => 'unlimited_linkedin_profile',
-		'youtube' => 'unlimited_youtube_profile',
-		'vimeo' => 'unlimited_vimeo_profile',
-		'tumblr' => 'unlimited_tumblr_profile',
-		'instagram' => 'unlimited_instagram_profile',
-		'flickr' => 'unlimited_flickr_profile',
-		'dribbble' => 'unlimited_dribbble_profile',
-		'rss' => 'unlimited_rss_profile',
-		'reddit' => 'unlimited_reddit_profile',
-		'soundcloud' => 'unlimited_soundcloud_profile',
-		'spotify' => 'unlimited_spotify_profile',
-		'vine' => 'unlimited_vine_profile',
-		'yahoo' => 'unlimited_yahoo_profile',
-		'behance' => 'unlimited_behance_profile',
-		'codepen' => 'unlimited_codepen_profile',
-		'delicious' => 'unlimited_delicious_profile',
-		'stumbleupon' => 'unlimited_stumbleupon_profile',
-		'deviantart' => 'unlimited_deviantart_profile',
-		'digg' => 'unlimited_digg_profile',
-		'git' => 'unlimited_git_profile',
-		'hacker-news' => 'unlimited_hacker-news_profile',
-		'steam' => 'unlimited_steam_profile',
-		'vk' => 'unlimited_vk_profile',
-		'weibo' => 'unlimited_weibo_profile',
-		'tencent-weibo' => 'unlimited_tencent_weibo_profile',
-		'email' => 'unlimited_email_profile'
+		'twitter' => 'author_twitter_profile',
+		'facebook' => 'author_facebook_profile',
+		'googleplus' => 'author_googleplus_profile',
+		'pinterest' => 'author_pinterest_profile',
+		'linkedin' => 'author_linkedin_profile',
+		'youtube' => 'author_youtube_profile',
+		'vimeo' => 'author_vimeo_profile',
+		'tumblr' => 'author_tumblr_profile',
+		'instagram' => 'author_instagram_profile',
+		'flickr' => 'author_flickr_profile',
+		'dribbble' => 'author_dribbble_profile',
+		'rss' => 'author_rss_profile',
+		'reddit' => 'author_reddit_profile',
+		'soundcloud' => 'author_soundcloud_profile',
+		'spotify' => 'author_spotify_profile',
+		'vine' => 'author_vine_profile',
+		'yahoo' => 'author_yahoo_profile',
+		'behance' => 'author_behance_profile',
+		'codepen' => 'author_codepen_profile',
+		'delicious' => 'author_delicious_profile',
+		'stumbleupon' => 'author_stumbleupon_profile',
+		'deviantart' => 'author_deviantart_profile',
+		'digg' => 'author_digg_profile',
+		'git' => 'author_git_profile',
+		'hacker-news' => 'author_hacker-news_profile',
+		'steam' => 'author_steam_profile',
+		'vk' => 'author_vk_profile',
+		'weibo' => 'author_weibo_profile',
+		'tencent-weibo' => 'author_tencent_weibo_profile',
+		'email' => 'author_email_profile'
 	);
 	return $social_sites;
 }
 
 // retrieves the attachment ID from the file URL
-function ct_unlimited_get_image_id($url) {
+function ct_author_get_image_id($url) {
 
     // Split the $url into two parts with the wp-content directory as the separator
     $parsed_url  = explode( parse_url( WP_CONTENT_URL, PHP_URL_PATH ), $url );
@@ -331,13 +321,13 @@ function ct_unlimited_get_image_id($url) {
     return $attachment[0];
 }
 
-function ct_unlimited_profile_image_output(){
+function ct_author_profile_image_output(){
 
     // use User's profile image, else default to their Gravatar
-    if(get_the_author_meta('unlimited_user_profile_image')){
+    if(get_the_author_meta('author_user_profile_image')){
 
         // get the id based on the image's URL
-        $image_id = ct_unlimited_get_image_id(get_the_author_meta('unlimited_user_profile_image'));
+        $image_id = ct_author_get_image_id(get_the_author_meta('author_user_profile_image'));
 
         // retrieve the thumbnail size of profile image
         $image_thumb = wp_get_attachment_image($image_id, 'thumbnail');
@@ -350,28 +340,28 @@ function ct_unlimited_profile_image_output(){
     }
 }
 
-function ct_unlimited_wp_backwards_compatibility() {
+function ct_author_wp_backwards_compatibility() {
 
 	// not using this function, simply remove it so use of "has_image_size" doesn't break < 3.9
 	if( get_bloginfo('version') < 3.9 ) {
 		remove_filter( 'image_size_names_choose', 'hybrid_image_size_names_choose' );
 	}
 }
-add_action('init', 'ct_unlimited_wp_backwards_compatibility');
+add_action('init', 'ct_author_wp_backwards_compatibility');
 
 /*
  * Set the date format for new users.
  * Needs to be done this way so that the date defaults to the right format, but can
  * still be changed from the Settings menu
  */
-function ct_unlimited_set_date_format() {
+function ct_author_set_date_format() {
 
 	// if the date format has never been set by Unlimited, set it
-	if( get_option('ct_unlimited_date_format_origin') != 'updated' ) {
+	if( get_option('ct_author_date_format_origin') != 'updated' ) {
 		update_option('date_format', 'F j, Y');
 
 		// add option so never updates date format again. Allows users to change format.
-		add_option('ct_unlimited_date_format_origin', 'updated');
+		add_option('ct_author_date_format_origin', 'updated');
 	}
 }
-add_action( 'init', 'ct_unlimited_set_date_format' );
+add_action( 'init', 'ct_author_set_date_format' );
