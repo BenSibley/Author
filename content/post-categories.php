821c2ca4edbf6016c13cfddc24544942b37a4da5
BenSibley <ben@competethemes.com>
2015-02-01 12:28:53 -0500

    updated categories output

--- content/post-categories.php
+++ content/post-categories.php
@@ -4,7 +4,7 @@
 $categories = get_the_category($post->ID);
 
 // comma-separate posts
-$separator = ', ';
+$separator = '';
 
 // create output variable
 $output = '';
@@ -12,15 +12,12 @@ $output = '';
 // if there are categories for the post
 if($categories){
 
-	echo '<p class="post-categories">';
+	echo '<div class="post-categories">';
+		echo '<span>Published in </span>';
 		foreach($categories as $category) {
-			// if it's the last and not the first (only) category, pre-prend with "and"
-			if( $category === end($categories) && $category !== reset($categories) ) {
-				$output .= 'and ';
-			}
 			// output category name linked to the archive
 			$output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s", 'author' ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
 		}
 		echo trim($output, $separator);
-	echo "</p>";
+	echo "</div>";
 }
