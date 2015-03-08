785bff1c090d050c0d1c4a1c6467e3d0019f393d
BenSibley <ben@competethemes.com>
2015-03-07 20:59:26 -0500

    removed async from primary js file

--- inc/scripts.php
+++ inc/scripts.php
@@ -8,7 +8,7 @@ function ct_author_load_scripts_styles() {
 	wp_register_style( 'ct-author-google-fonts', '//fonts.googleapis.com/css?family=Rokkitt:400|Lato:400');
 
 	// main JS file
-	wp_enqueue_script('ct-author-js', get_template_directory_uri() . '/js/build/production.min.js#ct_author_asyncload', array('jquery'),'', true);
+	wp_enqueue_script('ct-author-js', get_template_directory_uri() . '/js/build/production.min.js', array('jquery'),'', true);
 
 	// Google Fonts
 	wp_enqueue_style('ct-author-google-fonts');
