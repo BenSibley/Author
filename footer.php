b24aba4c812996b6d99f64db85b90456b849ef77
BenSibley <ben@competethemes.com>
2015-03-13 10:42:58 -0400

    target blank removed from footer link

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
         <span>
             <?php
                 $site_url = 'https://www.competethemes.com/author/';
-                $footer_text = sprintf( __( '<a target="_blank" href="%s">Author WordPress Theme</a> by Compete Themes', 'author' ), esc_url( $site_url ) );
+                $footer_text = sprintf( __( '<a href="%s">Author WordPress Theme</a> by Compete Themes', 'author' ), esc_url( $site_url ) );
                 $footer_text = apply_filters( 'ct_author_footer_text', $footer_text );
                 echo $footer_text;
             ?>
