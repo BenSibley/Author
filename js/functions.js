7deef09ad55c7dde11612cb17a32e4d7be276edf
BenSibley <ben@competethemes.com>
2015-03-15 16:31:26 -0400

    added fix for skip to content link bug in Chrome and IE9

--- js/functions.js
+++ js/functions.js
@@ -304,3 +304,19 @@ jQuery(document).ready(function($){
         }
     }
 });
+
+/* fix for skip-to-content link bug in Chrome & IE9 */
+window.addEventListener("hashchange", function(event) {
+
+    var element = document.getElementById(location.hash.substring(1));
+
+    if (element) {
+
+        if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+            element.tabIndex = -1;
+        }
+
+        element.focus();
+    }
+
+}, false);
