a082a6641c8bb14f9b2625b7de1e7a9f10c50bbc
BenSibley <ben@competethemes.com>
2015-05-12 10:04:20 -0400

    removed screen reader text from social icons and replaced with title attrbute on i

--- functions.php
+++ functions.php
@@ -408,22 +408,19 @@ if( ! function_exists('ct_author_social_icons_output') ) {
                     ?>
                     <li>
                         <a class="email" target="_blank" href="mailto:<?php echo antispambot( is_email( ct_author_get_social_url( $source, $key ) ) ); ?>">
-                            <span class="screen-reader-text">email icon</span>
-                            <i class="fa fa-envelope"></i>
+                            <i class="fa fa-envelope" title="<?php _e('email icon', 'author'); ?>"></i>
                         </a>
                     </li>
                 <?php } elseif ( $active_site == "flickr" || $active_site == "dribbble" || $active_site == "instagram" || $active_site == "soundcloud" || $active_site == "spotify" || $active_site == "vine" || $active_site == "yahoo" || $active_site == "codepen" || $active_site == "delicious" || $active_site == "stumbleupon" || $active_site == "deviantart" || $active_site == "digg" || $active_site == "hacker-news" || $active_site == "vk" || $active_site == 'weibo' || $active_site == 'tencent-weibo' ) { ?>
                     <li>
                         <a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( ct_author_get_social_url( $source, $key ) ); ?>">
-                            <span class="screen-reader-text"><?php echo $active_site; ?> icon</span>
-                            <i class="fa fa-<?php echo esc_attr( $active_site ); ?>"></i>
+                            <i class="fa fa-<?php echo esc_attr( $active_site ); ?>" title="<?php printf( __('%s icon', 'author'), $active_site ); ?>"></i>
                         </a>
                     </li>
                 <?php } else { ?>
                     <li>
                         <a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( ct_author_get_social_url( $source, $key ) ); ?>">
-                            <span class="screen-reader-text"><?php echo $active_site; ?> icon</span>
-                            <i class="fa fa-<?php echo esc_attr( $active_site ); ?>-square"></i>
+                            <i class="fa fa-<?php echo esc_attr( $active_site ); ?>-square" title="<?php printf( __('%s icon', 'author'), $active_site ); ?>"></i>
                         </a>
                     </li>
                 <?php
