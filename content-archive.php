9afe47bfac710701db36340d7c51bfd17fc1dec9
BenSibley <ben@competethemes.com>
2015-05-13 08:32:54 -0400

    date fix

--- content-archive.php
+++ content-archive.php
@@ -9,7 +9,7 @@
 			<span class="post-meta">
 				<?php
 				$author = "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "'>" . get_the_author() . "</a>";
-				$date = "<a href='" . get_month_link( get_the_date('Y'), get_the_date('n') ) . "'>" . date_i18n( get_option( 'date_format' ), strtotime( get_the_date() ) ) . "</a>";
+				$date = "<a href='" . get_month_link( get_the_date('Y'), get_the_date('n') ) . "'>" . date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) ) . "</a>";
 				?>
 				<?php printf( _x('Published by %1$s on %2$s', 'This blog post was published by some author on some date', 'author'), $author, $date ); ?>
 			</span>
