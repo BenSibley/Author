0d41d348f2335083936ad5faf7b4295580a5e349
BenSibley <ben@competethemes.com>
2015-07-20 10:43:28 -0400

    more tag now respected even if showing full post

--- functions.php
+++ functions.php
@@ -204,25 +204,22 @@ if( ! function_exists( 'ct_author_excerpt' ) ) {
         // make post variable available
         global $post;
 
-        // make 'read more' setting available
-        global $more;
-
         // check for the more tag
         $ismore = strpos( $post->post_content, '<!--more-->' );
 
         // get the show full post setting
         $show_full_post = get_theme_mod( 'full_post' );
 
-        // if show full post is on, show full post unless on search page
+	    // if show full post is on and not on a search results page
         if ( ( $show_full_post == 'yes' ) && ! is_search() ) {
 
-            // set read more value for all posts to 'off'
-            $more = - 1;
-
-            // output the full content
-            the_content();
+	        // use the read more link if present
+	        if ( $ismore ) {
+		        the_content( __( 'Continue reading', 'author' ) . " <span class='screen-reader-text'>" . get_the_title() . "</span>" );
+	        } else {
+		        the_content();
+	        }
         }
-
         // use the read more link if present
         elseif ( $ismore ) {
             the_content( __( 'Continue reading', 'author' ) . " <span class='screen-reader-text'>" . get_the_title() . "</span>" );
