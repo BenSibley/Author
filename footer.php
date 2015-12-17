e469f2336925a3d6a0070b8355e80c35d251412e
BenSibley <ben@competethemes.com>
2015-12-16 20:05:43 -0500

    simplified use of hard-coded URLs

--- footer.php
+++ footer.php
@@ -6,8 +6,7 @@
     <div class="design-credit">
         <span>
             <?php
-                $site_url = 'https://www.competethemes.com/author/';
-                $footer_text = sprintf( __( '<a href="%s">Author WordPress Theme</a> by Compete Themes', 'author' ), esc_url( $site_url ) );
+                $footer_text = sprintf( __( '<a href="%s">Author WordPress Theme</a> by Compete Themes', 'author' ), 'https://www.competethemes.com/author/' );
                 $footer_text = apply_filters( 'ct_author_footer_text', $footer_text );
                 echo wp_kses_post( $footer_text );
             ?>
