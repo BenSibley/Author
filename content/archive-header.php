04e134fc4992a0f50dd8a0dbc2c352b475b251ac
BenSibley <ben@competethemes.com>
2015-12-16 20:04:24 -0500

    now using WP core functions for archive titles and descriptions

--- content/archive-header.php
+++ content/archive-header.php
@@ -1,48 +1,24 @@
 <?php
-/* Category header */
-if( is_category() ){ ?>
-	<div class='archive-header'>
-		<i class="fa fa-folder-open"></i>
-		<h2>
-			<?php _e('Category archive for:', 'author'); ?>
-			<?php single_cat_title(); ?>
-		</h2>
-		<?php if ( category_description() ) echo category_description(); ?>
-	</div>
-<?php
+
+if ( ! is_archive() ) {
+	return;
 }
-/* Tag header */
-elseif( is_tag() ){ ?>
-	<div class='archive-header'>
-		<i class="fa fa-tag"></i>
-		<h2>
-			<?php _e('Tag archive for:', 'author'); ?>
-			<?php single_tag_title(); ?>
-		</h2>
-		<?php if ( tag_description() ) echo tag_description(); ?>
-	</div>
-<?php
+
+$icon_class = 'folder-open';
+
+if ( is_tag() ) {
+	$icon_class = 'tag';
+} elseif ( is_author() ) {
+	$icon_class = 'user';
+} elseif ( is_date() ) {
+	$icon_class = 'calendar';
 }
-/* Author header */
-elseif( is_author() ){ ?>
-	<div class='archive-header'>
-		<i class="fa fa-user"></i>
-		<h2>
-			<?php _e('Author archive for:', 'author'); ?>
-			<?php the_author_meta( 'display_name' ); ?>
-		</h2>
-		<?php if ( get_the_author_meta( 'description' ) ) echo '<p>' . get_the_author_meta( 'description' ) . '</p>'; ?>
-	</div>
-<?php
-}
-/* Date header */
-elseif( is_date() ){ ?>
-	<div class='archive-header'>
-		<i class="fa fa-calendar"></i>
-		<h2>
-			<?php _e('Date archive for:', 'author'); ?>
-			<?php single_month_title(' '); ?>
-		</h2>
-	</div>
-<?php
-}
+?>
+
+<div class='archive-header'>
+	<i class="fa fa-<?php echo $icon_class; ?>"></i>
+	<h2>
+		<?php the_archive_title(); ?>
+	</h2>
+	<?php the_archive_description(); ?>
+</div>
