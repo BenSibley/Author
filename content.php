70a14a015a9a62f4212d73b58f2c4622071b5f01
BenSibley <ben@competethemes.com>
2015-12-21 20:27:14 -0500

    moved post meta into content part

--- content.php
+++ content.php
@@ -4,13 +4,7 @@
 	<article>
 		<div class='post-header'>
 			<h1 class='post-title'><?php the_title(); ?></h1>
-			<span class="post-meta">
-				<?php
-				$author = "<a href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "'>" . get_the_author() . "</a>";
-				$date = "<a href='" . get_month_link( get_the_date('Y'), get_the_date('n') ) . "'>" . date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) ) . "</a>";
-				?>
-				<?php printf( _x('Published by %1$s on %2$s', 'This blog post was published by some author on some date', 'author'), $author, $date ); ?>
-			</span>
+			<?php get_template_part('content/post-meta'); ?>
 		</div>
 	    <div class="post-content">
 	        <?php the_content(); ?>
