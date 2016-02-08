0bef561681582098126f2e2b2740bae734061068
BenSibley <ben@competethemes.com>
2016-02-08 11:10:07 -0500

    comments closed message now always contained in comments section

--- comments.php
+++ comments.php
@@ -79,7 +79,9 @@ if ( comments_open() ) { ?>
 	</section>
 	<?php
 } else { ?>
-	<p class="comments-closed">
-		<?php _e( 'Comments are closed.', 'author' ); ?>
-	</p>
+	<section id="comments" class="comments">
+		<p class="comments-closed">
+			<?php _e( 'Comments are closed.', 'author' ); ?>
+		</p>
+	</section>
 <?php }
