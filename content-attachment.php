84e2425ac5f67624dc661102de65d196fea71c40
BenSibley <ben@competethemes.com>
2016-04-06 09:13:20 -0400

    attachment pages now show full size image, captions, and descriptions

--- content-attachment.php
+++ content-attachment.php
@@ -5,7 +5,17 @@
 			<h1 class='post-title'><?php the_title(); ?></h1>
 		</div>
 		<div class="post-content">
-			<?php the_content(); ?>
+			<?php
+			$image = wp_get_attachment_image($post->ID, 'full');
+			$image_meta = wp_prepare_attachment_for_js($post->ID);
+			?>
+			<div class="attachment-container">
+				<?php echo $image; ?>
+				<span class="attachment-caption">
+					<?php echo $image_meta['caption']; ?>
+				</span>
+			</div>
+			<?php echo wpautop( $image_meta['description'] ); ?>
 		</div>
 	</article>
 	<?php do_action( 'attachment_after' ); ?>
