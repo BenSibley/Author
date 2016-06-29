32abe3d75c71a46f945b8f00dd5951ffaaa0cb98
BenSibley <ben@competethemes.com>
2016-06-29 07:54:05 -0400

    simplified post pagination

--- content/post-nav.php
+++ content/post-nav.php
@@ -3,44 +3,51 @@
 global $post;
 
 $previous_post = get_adjacent_post( false, '', true );
-$next_post     = get_adjacent_post( false, '', false );
+$previous_text = __( 'Previous Post', 'author' );
 
-if ( $previous_post ) {
-	$previous_text  = __( 'Previous Post', 'author' );
-	$previous_title = get_the_title( $previous_post ) ? get_the_title( $previous_post ) : __( "The Previous Post", 'author' );
-	$previous_link  = get_permalink( $previous_post );
-} else {
+if ( $previous_post == '' ) {
 	$previous_text  = __( 'No Older Posts', 'author' );
-	$previous_title = __( 'Return to Blog', 'author' );
 	if ( get_option( 'show_on_front' ) == 'page' ) {
-		$previous_link = get_permalink( get_option( 'page_for_posts' ) );
+		$previous_url = get_permalink( get_option( 'page_for_posts' ) );
 	} else {
-		$previous_link = get_home_url();
+		$previous_url = get_home_url();
 	}
+	$previous_link = '<a href="' . esc_url( $previous_url ) . '">' . esc_html__( 'Return to Blog', 'author' ) . '</a>';
 }
 
-if ( $next_post ) {
-	$next_text  = __( 'Next Post', 'author' );
-	$next_title = get_the_title( $next_post ) ? get_the_title( $next_post ) : __( "The Next Post", 'author' );
-	$next_link  = get_permalink( $next_post );
-} else {
+$next_post  = get_adjacent_post( false, '', false );
+$next_text  = __( 'Next Post', 'author' );
+
+if ( $next_post == '' ) {
 	$next_text  = __( 'No Newer Posts', 'author' );
-	$next_title = __( 'Return to Blog', 'author' );
 	if ( get_option( 'show_on_front' ) == 'page' ) {
-		$next_link = get_permalink( get_option( 'page_for_posts' ) );
+		$next_url = get_permalink( get_option( 'page_for_posts' ) );
 	} else {
-		$next_link = get_home_url();
+		$next_url = get_home_url();
 	}
+	$next_link = '<a href="' . esc_url( $next_url ) . '">' . esc_html__( 'Return to Blog', 'author' ) . '</a>';
 }
 
 ?>
 <nav class="further-reading">
 	<div class="previous">
 		<span><?php echo esc_html( $previous_text ); ?></span>
-		<a href="<?php echo esc_url( $previous_link ); ?>"><?php echo esc_html( $previous_title ); ?></a>
+		<?php
+		if ( $previous_post == '' ) {
+			echo $previous_link;
+		} else {
+			previous_post_link( '%link' );
+		}
+		?>
 	</div>
 	<div class="next">
 		<span><?php echo esc_html( $next_text ); ?></span>
-		<a href="<?php echo esc_url( $next_link ); ?>"><?php echo esc_html( $next_title ); ?></a>
+		<?php
+		if ( $next_post == '' ) {
+			echo $next_link;
+		} else {
+			next_post_link( '%link' );
+		}
+		?>
 	</div>
 </nav>
