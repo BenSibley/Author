45430ebd573368c6d694c6b5cc117d93ca0d23a8
BenSibley <ben@competethemes.com>
2017-08-11 13:50:58 -0400

    review prompt now dismissing properly

--- dnh/handler.php
+++ dnh/handler.php
@@ -146,7 +146,7 @@ if ( ! class_exists( 'Dismissible_Notices_Handler' ) ) {
 		 * @return void
 		 */
 		private function includes() {
-			require( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'includes/helper-functions.php' );
+			require( trailingslashit( get_template_directory() ) . 'dnh/includes/helper-functions.php' );
 		}
 
 		/**
@@ -156,7 +156,7 @@ if ( ! class_exists( 'Dismissible_Notices_Handler' ) ) {
 		 * @return void
 		 */
 		public function load_script() {
-			wp_register_script( 'dnh', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'assets/js/main.js', array( 'jquery' ), self::$instance->version, true );
+			wp_register_script( 'dnh', trailingslashit( get_template_directory_uri() ) . 'dnh/assets/js/main.js', array( 'jquery' ), self::$instance->version, true );
 			wp_enqueue_script( 'dnh' );
 		}
 
