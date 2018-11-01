f61992a21433ebd11f7e1d4a942d915a8bb30248
BenSibley <ben@competethemes.com>
2018-11-01 14:45:10 -0400

    fixed FA stylesheet URL

--- inc/scripts.php
+++ inc/scripts.php
@@ -19,7 +19,7 @@ function ct_author_load_scripts_styles() {
 		'closeChildMenu'   => esc_html__( 'close child menu', 'author' )
 	) );
 
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
+	wp_enqueue_style( 'ct-author-font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/all.min.css' );
 
 	wp_enqueue_style( 'ct-author-style', get_stylesheet_uri() );
 
