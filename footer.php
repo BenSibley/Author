58ca71a8a897272b190268161f37e8812d15b742
BenSibley <ben@competethemes.com>
2020-07-14 13:50:37 -0400

    shortcode can now be output in footer text section

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
             <?php
             $footer_text = sprintf( __( '<a href="%1$s">%2$s WordPress Theme</a> by Compete Themes', 'author' ), 'https://www.competethemes.com/author/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_author_footer_text', $footer_text );
-            echo wp_kses_post( $footer_text );
+            echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
         </span>
 	</div>
